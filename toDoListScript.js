9fc5152218b1c84ce0487b8e7dbc4ceb370dd904
lilgujj <gustavkarlandersson@hotmail.com>
2020-11-18 19:51:37 +0100

    another try!!! big brain

--- toDoListScript.js
+++ toDoListScript.js
@@ -6,10 +6,15 @@ function eventListenersButtons(){
     remove.onclick = removeLastItemFromList; // ANROPA removeLastItemFromList FUNKTION NÄR KLICK HÄNDER
  }
  // Lägger till 
+ // Array till vad användaren skriver som input som sedan visas på sidan
+ let inputArray = []; 
+
  function takeInput(){
-     const input = document.getElementById('addItem').value; // Tar texten från input
-     console.log("hej")
-     addNewItemToList(input); // skickar input texten i en const input variabel.
+     console.log("klickade på Lägg Till")
+    let input = document.getElementById('addItem').value;   // Tar texten från input
+    inputArray.push(input);
+    console.log(inputArray);
+    addNewItemToList(input); // skickar input texten i en const input variabel.
  }
  
  function addNewItemToList(input){ 
@@ -23,24 +28,54 @@ function eventListenersButtons(){
          }
          
  }
-
+// Ta bort det som står i input med samma som står som ett list item.
+// ta bort just det list itemet
+// 
 function removeLastItemFromList() {
+    console.log("Klickade på Ta Bort")
     const input = document.getElementById('addItem').value; //const or let
-    removeNewItemFromList(input);
-}
 
-// Input is what the user writes in the box
-
-function removeNewItemFromList(input){
-        const ul = document.querySelector('ul');
-        const li = document.removeElement('li');
-        li.innerText = input;
-        ul.removeChild(li) 
-        if (input === "") {
-            li.parentNode.removeChild(li)
+    for (i=0; i < inputArray.length; i++){
+        if (inputArray[i] === input) {
+            console.log("Remove Li Child element with this input name");
+            inputArray.p // här skall jag ta bort input valuet ur inputarray för jag tryckte nyss på ta bort.
+            // sedan får jag försöka hitta ett sätt att presentera en array på en sida så är de löst.
+            console.log(inputArray);
+        } else {
+            alert("Det finns ingen todo med detta namn! prova igen")
         }
 
+    }
+    // removeNewItemFromList(input); First attempt. read more below.
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// Input is what the user writes in the box
+//  First  attempt to reverse the add style to it. but did now work
+// function removeNewItemFromList(input){
+//         const ul = document.querySelector('ul');
+//         const li = document.removeElement('li');
+//         if (li.innerText = input) {
+//             console.log("JA")
+//             ul.removeChild(li) 
+//         }
+//         li.innerText = input;
+//         ul.removeChild(li) 
+//         if (input === "") {
+//             li.parentNode.removeChild(li)
+//         }
+
+// }
 
 
  // 
