5e2ddf2154c6666cd2a4623c4a8dedd7f4956f64
Intradastingly <asaf_lackgren@hotmail.com>
2020-11-20 10:39:13 +0100

    fixed conflics

--- scripts/toDoListScript.js
+++ scripts/toDoListScript.js
@@ -1,29 +1,36 @@
+
+
 // Add eventlisteners to Buttons.
 function eventListenersButtons(){
-    const confirm = document.getElementById('confirmAddItem'); 
-    confirm.onclick = takeInput; 
+   const confirm = document.getElementById('confirmAddItem'); 
+   confirm.onclick = takeInput; 
 
-    const remove = document.getElementById('removeItem'); 
-    remove.onclick = removeItemFromList;
- }
+   const remove = document.getElementById('removeItem'); 
+   remove.onclick = removeItemFromList;
+}
 
- function takeInput(){
-    let input = document.getElementById('addItem').value;
-    addNewItemToList(input);
- }
 
- // adds value to new li as textNode. 
- function addNewItemToList(input){
-    const ul = document.getElementById("ul");
-    const li = document.createElement("li");
-    li.setAttribute("id", input);
-    li.appendChild(document.createTextNode(input)); // gives all li differents textNodes with input text.
-    ul.appendChild(li);
- }
+// takes input text as value.
+function takeInput(){
+   let input = document.getElementById('addItem').value;
+   addNewItemToList(input);
+   
+}
 
- // removes li wich has the input text value.
-function removeItemFromList() {
-    const ul = document.getElementById("ul");
+// adds value to new li as textNode. 
+function addNewItemToList(input){
+   const ul = document.getElementById("ul-items");
+   const li = document.createElement("li");
+   li.setAttribute("id", input);
+   li.appendChild(document.createTextNode(input)); // gives all li differents textNodes with input text.
+   ul.appendChild(li);
+
+}
+
+// removes li wich has the input text value.
+function removeItemFromList(input) {
+    const ul = document.getElementById("ul-items");
     const item = document.getElementById(addItem.value);
     ul.removeChild(item);
 }
+
