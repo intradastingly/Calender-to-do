ffcc25cb9adb36f54c32753f6675fc643d6f0b9f
Macbook <Macbook@Macbooks-MBP-2.lan>
2020-11-23 11:07:31 +0100

    Added fetch API and filtering out  holidays

---
+++ scripts/holidays.js
@@ -0,0 +1,37 @@
+// Starta programmet
+// Hämta dagar
+// Filtrera ut helgdagar
+
+/** Starts the program */
+window.addEventListener('load',fetchAllDays);
+
+/** Fetch all days from the API for year 2020 */
+async function fetchAllDays() {
+    try {
+        const url = 'https://api.dryg.net/dagar/v2.1/2020';
+        const result = await fetch(url);
+        const data = await result.json();
+
+        getHolidays(data);
+    }
+        
+     catch (error) {
+        console.error(error);
+    }
+    
+}
+/** Filter out holidays and push them to an array  */
+function getHolidays (data) {
+    const holidays = [];
+
+    for (day in data.dagar) {
+        if (day.helgdag) {
+            holidays.push(day);
+            
+        }
+        /*console.log(holidays);*/
+        
+    }
+
+}
+
